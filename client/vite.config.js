9b9f78f78515548525a643e2608913f6d541a28c
Jose Carlos <jmtiascarlos2018@gmail.com>
2026-06-11 13:49:27 -0300

    Fix: ajusta outDir para dist no deploy do Netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
@@ -15,7 +14,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     emptyOutDir: true,
   },
 })
